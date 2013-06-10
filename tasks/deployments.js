bb490805fe7b062b2440267c277fca219fe3e540
David <getdavemail@gmail.com>
2013-06-10 09:54:23 +0100

    Initial Commit

---
+++ tasks/deployments.js
@@ -0,0 +1,25 @@
+/*
+ * grunt-deployments
+ * https://github.com/getdave/grunt-deployments
+ *
+ * Copyright (c) 2013 David Smith
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('deployments', 'Push and pull databases', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+   
+    });
+
+    
+  });
+
+};
