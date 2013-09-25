9e87994fd4dfba506bf5ccb8a1b3fb512a8d5f97
David <getdavemail@gmail.com>
2013-09-25 12:47:42 +0100

    Integrate Chai expect style assertions. Add x2 dummy tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,8 @@
 
 module.exports = function(grunt) {
 
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
   // Project configuration.
   grunt.initConfig({
@@ -31,33 +33,34 @@ module.exports = function(grunt) {
       tests: ['tmp'],
     },
 
-    deployments: {
+    simplemocha: {
         options: {
-            backups_dir: ''
+            globals: ['chai'],
+            reporter: 'Nyan'
         },
-        local: '<%= db_fixture.local %>',
-        develop: '<%= db_fixture.develop %>'
-    },
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
+        all: { 
+            src: [
+              'test/**/*.js'
+            ] 
+        }
+    }
 
   });
 
 
-  // Actually load this plugin's task(s).
+/*   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('chai');
+  grunt.loadNpmTasks('grunt-simple-mocha'); */
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'deployments', 'nodeunit']);
+  grunt.registerTask('test', ['clean', 'simplemocha']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
